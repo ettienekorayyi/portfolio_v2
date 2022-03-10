f5b956778b34279f22c0a96b1210b448f438014f
ettienekorayyi <corralstephenmelben@gmail.com>
2022-03-11 08:52:47 +1100

    Implemented Layout Structuring of the webpage.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
+import { Home } from "./home/Home";
 
 function App() {
   return (
     <div className="App">
-      <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Home />      
     </div>
   );
 }
---
+++ src/components/navigation/HoverSideNav.js
@@ -0,0 +1,14 @@
+import './HoverSideNav.css';
+
+export const HoverSideNav = () => {
+    return (
+        <div id="HoverSideNav" className="SideNav">
+            <a href="#" id="home">Home</a>
+            <a href="#" id="about">About</a>
+            <a href="#" id="experience">Experience</a>
+            <a href="#" id="projects">Projects</a>
+            <a href="#" id="contact">Contact</a>
+        </div>
+    );
+}
+
