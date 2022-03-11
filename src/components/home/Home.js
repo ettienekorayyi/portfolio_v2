1a8958f13be45b1bee720f280c6d7242d6327704
ettienekorayyi <corralstephenmelben@gmail.com>
2022-03-11 21:03:18 +1100

    Added Name and Title at root page.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,9 @@
 import React from "react";
 import './Home.css';
 import { HoverSideNav } from "../navigation/HoverSideNav";
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';;
-
 
 
 export const Home = () => {
@@ -13,8 +13,16 @@ export const Home = () => {
         <CssBaseline />
         <HoverSideNav />
         <Container maxWidth="lg">
-
-
+          <Box sx={{ height: '100vh' }}>
+            <div className="content">
+              <div className="hero_header">
+                <h1>Stephen Melben Corral</h1>
+              </div>
+              <div className="text">
+                <h1>A Fullstack Software Engineer.</h1>
+              </div>
+            </div>
+          </Box>
         </Container>
       </div>
     </React.Fragment>
