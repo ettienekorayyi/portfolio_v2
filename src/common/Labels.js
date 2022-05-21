8e0efcee7b531f6550ba2d1439b3a5cef8379b65
ettienekorayyi <corralstephenmelben@gmail.com>
2022-05-22 01:03:40 +1000

    Created Labels js file to ensure the texts are easily manageable.

---
+++ src/common/Labels.js
@@ -0,0 +1,10 @@
+export const BriefSummaryTitleLabels = () => {
+    return <h1>A Brief Summary of Myself</h1>;
+}
+
+export const BriefSummaryContentLabels = () => {
+    return <p>Hi folks, I'm Stephen, I love building, breaking, and rebuilding stuff.
+        I enjoy software development and I'm currently engaged in IT volunteer at
+        Code.Sydney where I assisted them with web development with 3 of their
+        projects in which, two of those projects I led.</p>;
+}
