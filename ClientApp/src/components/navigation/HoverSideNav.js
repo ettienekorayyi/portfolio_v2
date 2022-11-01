0630008b704217fc0f2fbf0dd6a9ac8a24d5b438
ettienekorayyi <corralstephenmelben@gmail.com>
2022-11-01 20:03:38 +1100

    Fixed text spacing

--- ClientApp/src/components/navigation/HoverSideNav.js
+++ ClientApp/src/components/navigation/HoverSideNav.js
@@ -9,13 +9,11 @@ import { smoothScroll } from '../../utils/scroll';
 
 export const HoverSideNav = ({about, contact, home}) => {
    
-    //console.log(contact.current)
-
     return (
         <div className="sidenav">
             <a href="#" id="home" onClick={() => smoothScroll(home.current)}>Home <HomeIcon id="homeIcon" /></a>
             <a href="#" id="about" onClick={() => smoothScroll(about.current)}>About <PersonIcon id="personIcon" /></a>
-            <a href="#" id="experience" >Work <BusinessCenterIcon id="workIcon" /></a>
+            {/* <a href="#" id="experience" >Work <BusinessCenterIcon id="workIcon" /></a> */}
             <a href="#" id="projects" >Projects <LaptopIcon id="projectsIcon" /></a>
             <a href="#" id="contact" onClick={() => smoothScroll(contact.current)}>Contact <TelegramIcon id="telegramIcon" /></a>
         </div>
