cef577fece72f7e56864066c5afc500baaab37e9
ettienekorayyi <corralstephenmelben@gmail.com>
2022-11-05 17:58:39 +1100

    Added projects component for desktop view

---
+++ ClientApp/src/api/projects.js
@@ -0,0 +1,27 @@
+import lukas from '../img/lukas-carey-logo.png';
+import eidsteddfod from '../img/parramatta-eidsteddfod.png'; 
+import taskstech from '../img/taskstech-logo.png';
+
+export const projects = [
+    {
+        id: 1,
+        photo: lukas,
+        url: "https://www.lukascarey.com.au/",
+        title: "Project Lukas",
+        description: "Dr. Lukas Carey is a strong advocate for the importance of Convict Criminology and the lived experience to govern the direction of related policies and practices."
+    },
+    {
+        id: 2,
+        photo: eidsteddfod,
+        url: "https://www.eisteddfodparramatta.org.au/",
+        title: "Parramatta Eisteddfod",
+        description: `Parramatta Eisteddfod is a not-for-profit organisation of volunteers whose aim is to provide a safe and encouraging performance arts event for the young people.`
+    },
+    {
+        id: 3,
+        photo: taskstech,
+        url: "https://www.taskstech.com/login",
+        title: "Project TasksTech",
+        description: `TasksTech is a niche alternative web application developed to help tradies manage their customer jobs and record diaries with a capability to upload photos.`
+    }
+];
