8d5ed190b4e3bb4f449ac8d322a67e6fa6d81e9a
ettienekorayyi <corralstephenmelben@gmail.com>
2025-05-18 12:23:21 +1000

    Fixed navigation gap

--- ClientApp/src/api/projects.js
+++ ClientApp/src/api/projects.js
@@ -16,12 +16,5 @@ export const projects = [
         url: "https://www.eisteddfodparramatta.org.au/",
         title: "Parramatta Eisteddfod",
         description: `Parramatta Eisteddfod is a not-for-profit organisation of volunteers whose aim is to provide a safe and encouraging performance arts event for the young people.`
-    },
-    {
-        id: 3,
-        photo: taskstech,
-        url: "https://www.taskstech.com/login",
-        title: "Project TasksTech",
-        description: `TasksTech is a niche alternative web application developed to help tradies manage their customer jobs and record diaries with a capability to upload photos.`
     }
 ];
